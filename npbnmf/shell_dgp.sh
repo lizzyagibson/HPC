d1adb4b9c0163ead9a2f7f92c4900a0c1ee0a3c1
Elizabeth Gibson %MSPH_EHS <eag2186@login2.hpc>
2020-11-02 18:40:46 -0500

    rerun 100x.

---
+++ npbnmf/shell_dgp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -cwd -S /bin/bash
+#$ -o /dev/null
+#$ -e /dev/null
+#$ -l mem=12G
+#$ -l time=:10000:
+#$ -M eag2186@cumc.columbia.edu
+
+$MODULESHOME/init/bash
+module load matlab/2018a
+
+clear
+
+matlab -nojvm -nodisplay -nosplash  -singleCompThread -nodesktop  -logfile "/ifs/scratch/msph/ehs/eag2186/npbnmf/log-mat{$SGE_TASK_ID}" -r "run /ifs/scratch/msph/ehs/eag2186/npbnmf/loop_dgp.m"
+
