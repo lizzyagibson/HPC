467c592e0730da1538b91e43a0cdb92b43942e19
Elizabeth Gibson %MSPH_EHS <eag2186@login3.hpc>
2021-01-22 13:40:48 -0500

    Run bootstrap in correlated sims.

--- npbnmf/bootstrap_bn2mf.sh
+++ npbnmf/bootstrap_bn2mf.sh
@@ -11,4 +11,4 @@ module load matlab/2018a
 
 clear
 
-matlab -nojvm -nodisplay -nosplash  -singleCompThread -nodesktop  -logfile "/ifs/scratch/msph/ehs/eag2186/npbnmf/log/log-bs{$SGE_TASK_ID}" -r "run /ifs/scratch/msph/ehs/eag2186/npbnmf/bootstrap_bn2mf.m"
+matlab -nojvm -nodisplay -nosplash  -singleCompThread -nodesktop  -logfile "/ifs/scratch/msph/ehs/eag2186/npbnmf/log/bs{$SGE_TASK_ID}" -r "run /ifs/scratch/msph/ehs/eag2186/npbnmf/bootstrap_bn2mf.m"
